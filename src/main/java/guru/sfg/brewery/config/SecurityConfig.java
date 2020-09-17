a9a03b589ccb1c4b3f522f19b661b93d974f33f5
Sam Lau <samlau.sam@gmail.com>
2020-09-17 09:02:37 +0800

    updated for less convoluted codes

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -11,21 +11,43 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
+	// this setup the Authorization (access to assets); 
+	// override configure(AuthenticationManagerBuilder) to setup Authentication (logins)
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/","/webjars/**","/login","/resources/**").permitAll()
+				.antMatchers("/beers/find","/beers").permitAll()
+				.antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
+				.mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll() //note we use mvcMatcher; works the same
+				.anyRequest().authenticated()
+		.and()
+			.formLogin()
+		.and()
+			.httpBasic(); //use httpBasic authorization (Basic Auth), not secure unless use https
+	}
+/*  alternate way to code the above, seems more convoluted	
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests(authorize -> { //no authorization needed to access the following assets
 				authorize
-					.antMatchers("/","/webjars/**","/login","/resources/**").permitAll()
-					.antMatchers("/beers/find","/beers").permitAll()
-					.antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
-					.mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll(); //note we use mvcMatcher; works the same
+				.antMatchers("/","/webjars/**","/login","/resources/**").permitAll()
+				.antMatchers("/beers/find","/beers").permitAll()
+				.antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
+				.mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll(); //note we use mvcMatcher; works the same
 			})
 			.authorizeRequests() //any other resource access will need authorization
-				.anyRequest().authenticated()
-			.and()
+			.anyRequest().authenticated()
+		.and()
 			.formLogin()
-			.and()
+		.and()
 			.httpBasic(); //use httpBasic authorization (Basic Auth), not secure unless use https
 	}
-
+*/	
+	
+	
+	
+	
+	
 }
